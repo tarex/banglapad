48abd7e93ef86be414b40566d420eabc89a526c4
Tareq Jobayere <tarexyz@gmail.com>
2021-06-28 23:19:42 +0600

    added again

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,23 +1,30 @@
-(function($) {
-	$.extend( $.ime.languages, {
-		'bn': {
-			autonym: 'বাংলা',
-			inputmethods: [ 'bn-avro']
-		},
-	});
-  $.ime.preferences.setLanguage( "bn" );
-	$.ime.preferences.setIM('bn-avro');
-	$("#banglapad").ime();
+(function ($) {
+  $.extend($.ime.languages, {
+    bn: {
+      autonym: 'বাংলা',
+      inputmethods: ['bn-avro'],
+    },
+  });
+  $.ime.preferences.setLanguage('bn');
+  $.ime.preferences.setIM('bn-avro');
+  $('#banglapad').ime();
   new Clipboard('.btn');
 
-  $('.shareButton').click(function() {
+  $('.shareButton').on('click', function () {
     const message = $('#banglapad').val();
     if (message.length) {
-      FB.login(function(){
-  			FB.api('/me/feed', 'post', { message });
-      }, {scope: 'publish_actions'});
+      FB.login(
+        function () {
+          FB.api('/me/feed', 'post', { message });
+        },
+        { scope: 'publish_actions' }
+      );
     } else {
       alert('কিছু নাই, শেয়ার হবে না');
     }
+  });
+
+  $('.saveButton').on('click', function () {
+    alert('yo');
   });
 })(jQuery);
